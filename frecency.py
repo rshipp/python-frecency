fa5d980a6c71c18fc45fe255acfc724e194f6ab0
george <rpubaddr0@gmail.com>
2014-07-12 21:37:45 -0600

    Docstring modifications.

--- frecency.py
+++ frecency.py
@@ -2,11 +2,9 @@ from __future__ import division
 import math
 
 def score_item(recencies, time_constant):
-    """Calculates the absolute score.
+    """Calculates the absolute score of a given item.
 
-    :param recencies: an iterable containing time offsets from current
-                      time
-    :param time_constant: a number that should not be less than or equal
-                          to zero, unless you want strange results
+    :param recencies: an iterable containing int time offsets from the current time
+    :param time_constant: numeric constant, should be > 0 in most cases
     """
     return sum(math.exp(-r / time_constant) for r in recencies)

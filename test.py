3bf4c3475aff686e43dba65482d2f34bc423acc3
n8many <n8nyoung@gmail.com>
2014-07-13 02:42:57 +0000

    Switched from delta to places, Python 2.6 compat.

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import frecency
 class TestFrecency(unittest.TestCase):
 
     def test_weight_assigned_correctly(self):
-        self.assertAlmostEquals(2.9557,
+        self.assertAlmostEquals(2.955684590875269,
                 frecency.score_item([50,30,600,200],600), 
-                delta=0.001)
+                places=10)
          

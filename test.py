4c580a1a8bb04df1916de2f9034cbe36145fea5e
george <rpubaddr0@gmail.com>
2014-07-20 15:32:37 -0600

    Fixed spacing in first test.

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ class TestFrecency(unittest.TestCase):
 
     def test_weight_assigned_correctly(self):
         self.assertAlmostEquals(2.955684590875269,
-                frecency.score_item([50,30,600,200],600), 
+                frecency.score_item([50, 30, 600, 200], 600),
                 places=10)
 
     def test_raises_error_on_invalid_input(self):

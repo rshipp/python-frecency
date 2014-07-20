e905a4a453326b4407fadbe54f13dfcf0ef45881
george <rpubaddr0@gmail.com>
2014-07-20 15:21:05 -0600

    Added empty test definitions.

--- test.py
+++ test.py
@@ -7,4 +7,16 @@ class TestFrecency(unittest.TestCase):
         self.assertAlmostEquals(2.955684590875269,
                 frecency.score_item([50,30,600,200],600), 
                 places=10)
-         
+
+    def test_raises_error_on_invalid_input(self):
+        pass
+
+    def test_empty_list_returns_none(self):
+        pass
+
+    def test_negative_recency_works(self):
+        pass
+
+    def test_zero_time_constant_raises_exception(self):
+        pass
+
